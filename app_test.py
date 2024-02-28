9b0c2f915b36406b21a4b379eacf15c4555d693d
RohanRajure (RG) <99001753+RajureRohan1256@users.noreply.github.com>
2024-02-28 22:52:13 +0530

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -14,7 +14,6 @@ st.write(
   ''' OpenAI has released several versions of its Generative Pre-trained Transformer (GPT) models,
   including GPT-3, which is the latest version available up to that point. These models are designed to
    understand and generate human-like text based on the input they receive.
-
   '''
 )
 file_uploaded = st.file_uploader('Upload you PDF Here',type=['pdf'])
@@ -25,88 +24,88 @@ st.title("Let's start QnAs")
 
 
 # provide the path of  pdf file/files.
-pdfreader = PdfReader(file_uploaded)
-
-
-#if file_uploaded == True:
-#  pdfreader = PdfReader(file_uploaded)
-
-
-from typing_extensions import Concatenate
-# read text from pdf
-raw_text = ''
-for i, page in enumerate(pdfreader.pages):
-    content = page.extract_text()
-    if content:
-        raw_text += content
-
-# We need to split the text using Character Text Split such that it sshould not increse token size
-text_splitter = CharacterTextSplitter(
-    separator = "\n",
-    chunk_size = 800,
-    chunk_overlap  = 200,
-    length_function = len,
-)
-texts = text_splitter.split_text(raw_text)
-
-# We need to split the text using Character Text Split such that it sshould not increse token size
-text_splitter = CharacterTextSplitter(
-    separator = "\n",
-    chunk_size = 800,
-    chunk_overlap  = 200,
-    length_function = len,
-)
-texts = text_splitter.split_text(raw_text)
-
-
-
-# Download embeddings from OpenAI
-embeddings = OpenAIEmbeddings()
-
-document_search = FAISS.from_texts(texts, embeddings)
-
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-
-chain = load_qa_chain(OpenAI(), chain_type="stuff")
-
-
-## Creating ML app
-
-#import streamlit as st
-
-
-
-#while True:
-#query1 = st.text_area(label='Your Prmpt Here', height=100, placeholder='Prompt')
-
-#query = st.text_input('Your Prompt', query)
-
-query1 = st.text_area(
-  label = 'Display Prompt',
-  height= 100,
-  max_chars = 40,
-  placeholder = "Prompt Here"
-)
-
-st.write('Your Prompt is : ', query1)
-
-docs = document_search.similarity_search(query1)
-answer1 = chain.run(input_documents=docs, question=query1)
-
-st.title('Answer:')
-st.write(answer1)
-
-#ans_button = st.button('Generate Answer', on_click=answer1, use_container_width=False)
-
-#if ans_button == True:
-#  st.write(answer1)
-
-
-
-##ignore
-
-#generate_ans_button = st.button('Generate Answer')
-#st.write(generate_ans_button)
-#if generate_ans_button==True:
-#  st.write(query)
+if pdfreader = PdfReader(file_uploaded) == Ture:
+  #if file_uploaded == True:
+  #  pdfreader = PdfReader(file_uploaded)
+  
+  
+  from typing_extensions import Concatenate
+  # read text from pdf
+  raw_text = ''
+  for i, page in enumerate(pdfreader.pages):
+      content = page.extract_text()
+      if content:
+          raw_text += content
+  
+  # We need to split the text using Character Text Split such that it sshould not increse token size
+  text_splitter = CharacterTextSplitter(
+      separator = "\n",
+      chunk_size = 800,
+      chunk_overlap  = 200,
+      length_function = len,
+  )
+  texts = text_splitter.split_text(raw_text)
+  
+  # We need to split the text using Character Text Split such that it sshould not increse token size
+  text_splitter = CharacterTextSplitter(
+      separator = "\n",
+      chunk_size = 800,
+      chunk_overlap  = 200,
+      length_function = len,
+  )
+  texts = text_splitter.split_text(raw_text)
+  
+  
+  
+  # Download embeddings from OpenAI
+  embeddings = OpenAIEmbeddings()
+  
+  document_search = FAISS.from_texts(texts, embeddings)
+  
+  from langchain.chains.question_answering import load_qa_chain
+  from langchain.llms import OpenAI
+  
+  chain = load_qa_chain(OpenAI(), chain_type="stuff")
+  
+  
+  ## Creating ML app
+  
+  #import streamlit as st
+  
+  
+  
+  #while True:
+  #query1 = st.text_area(label='Your Prmpt Here', height=100, placeholder='Prompt')
+  
+  #query = st.text_input('Your Prompt', query)
+  
+  query1 = st.text_area(
+    label = 'Display Prompt',
+    height= 100,
+    max_chars = 40,
+    placeholder = "Prompt Here"
+  )
+  
+  st.write('Your Prompt is : ', query1)
+  
+  docs = document_search.similarity_search(query1)
+  answer1 = chain.run(input_documents=docs, question=query1)
+  
+  st.title('Answer:')
+  st.write(answer1)
+  
+  #ans_button = st.button('Generate Answer', on_click=answer1, use_container_width=False)
+  
+  #if ans_button == True:
+  #  st.write(answer1)
+  
+  
+  
+  ##ignore
+  
+  #generate_ans_button = st.button('Generate Answer')
+  #st.write(generate_ans_button)
+  #if generate_ans_button==True:
+  #  st.write(query)
+else:
+  break
